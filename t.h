dedb5bf79cdc0932932ddec146b6379d864efe2e
Jordan Majd <jordan@madtitanstudios.com>
2017-04-08 13:21:47 -0600

    Add function definition to header

--- t.h
+++ t.h
@@ -1,1 +1,23 @@
+#ifndef T_H
+#define T_H
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+/*** terminal ***/
+
+void die(const char*);
+void disableRawMode();
+void enableRawMode();
+char editorReadKey();
+
+/*** output ***/
+
+void editorRefreshScreen();
 void editorClearScreen();
+
+/*** input ***/
+
+void editorProcessKeypress();
+
+
+#endif
