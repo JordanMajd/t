9b8c3989b6fcbad93db72f4740c4726d0429bde5
Jordan Majd <jordan@madtitanstudios.com>
2017-04-08 13:02:01 -0600

    clear screen on exit

--- t.c
+++ t.c
@@ -18,6 +18,10 @@ struct termios orig_termios;
 /*** terminal ***/
 
 void die(const char *s){
+
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
 	perror(s);
 	exit(1);
 }
