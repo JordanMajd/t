fa063d1677a9be89433eefd7d43c06fcbc00d359
Jordan Majd <jordan@madtitanstudios.com>
2017-04-08 13:44:05 -0600

    Add global state, refactor global to be in header

--- t.c
+++ t.c
@@ -1,20 +1,4 @@
-/*** includes ***/
-
-#include <ctype.h>
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include "t.h"
-#include <termios.h>
-#include <unistd.h>
-
-/*** defines ***/
-
-#define CTRL_KEY(k) ((k) & 0x1f)
-
-/*** data ***/
-
-struct termios orig_termios;
 
 /*** terminal ***/
 
@@ -25,20 +9,20 @@ void die(const char *s){
 }
 
 void disableRawMode(){
-	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1){
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1){
 		die("tcsetattr");
 	}
 }
 
 void enableRawMode(){
 
-	if(tcgetattr(STDIN_FILENO, &orig_termios) == -1){
+	if(tcgetattr(STDIN_FILENO, &E.orig_termios) == -1){
 		die("tcgetattr");
 	}
 
 	atexit(disableRawMode);
 
-	struct termios raw = orig_termios;
+	struct termios raw = E.orig_termios;
 
 	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP| IXON);
 	raw.c_oflag &= ~(OPOST);
--- t.h
+++ t.h
@@ -1,7 +1,26 @@
 #ifndef T_H
 #define T_H
 
+/*** includes ***/
+
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+/*** macros ***/
+
 #define CTRL_KEY(k) ((k) & 0x1f)
+
+/*** data ***/
+
+struct editorConfig{
+	struct termios orig_termios;
+};
+
+struct editorConfig E;
 
 /*** terminal ***/
 
