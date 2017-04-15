57017a18e7c72920227ce4827ee1b494ffddf549
Jordan Majd <jordan@madtitanstudios.com>
2017-04-15 11:42:45 -0600

    Add modified status if dirty

--- src/t.c
+++ src/t.c
@@ -166,7 +166,9 @@ void editorAppendRow(char *s, size_t len){
 	E.row[at].rsize = 0;
 	E.row[at].render = NULL;
 	editorUpdateRow(&E.row[at]);
+	
 	E.numrows++;
+	E.dirty++;
 }
 
 //cursor hops tablength
@@ -193,6 +195,7 @@ void editorRowInsertChar(erow *row, int at, int c) {
 	row->size++;
 	row->chars[at] = c;
 	editorUpdateRow(row);
+	E.dirty++;
 }
 
 void editorUpdateRow(erow *row){
@@ -264,6 +267,7 @@ void editorOpen(char *filename){
 	}
 	free(line);
 	fclose(fp);
+	E.dirty = 0;
 }
 
 void editorSave() {
@@ -282,6 +286,7 @@ void editorSave() {
 			if(write(fd, buf, len) == len) {
 				close(fd);
 				free(buf);
+				E.dirty = 0;
 				editorSetStatusMessage("%d bytes written to disk", len);
 				return;
 			}
@@ -393,7 +398,7 @@ void editorDrawStatusBar(struct abuf *ab){
 	char status[80];
 	char rstatus[80];
 
-	int len = snprintf(status, sizeof(status), "%.20s - %d lines", E.filename ? E.filename : "[No Name]", E.numrows);
+	int len = snprintf(status, sizeof(status), "%.20s - %d lines %s", E.filename ? E.filename : "[No Name]", E.numrows, E.dirty ? "(modified)" : "");
 
 	int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", E.cy + 1, E.numrows);
 
@@ -596,6 +601,7 @@ void initEditor(){
 	E.coloff = 0;
 	E.numrows = 0;
 	E.row = NULL;
+	E.dirty = 0;
 	E.filename = NULL;
 	E.statusmsg[0] = '\0';
 	E.statusmsg_time = 0;
--- src/t.h
+++ src/t.h
@@ -61,6 +61,7 @@ struct editorConfig{
 	int screencols;
 	int numrows;
 	erow *row;
+	int dirty;
 	char *filename;
 	char statusmsg[80];
 	time_t statusmsg_time;
